7c8eb1cc3ce90b8edf42af6f1a4559a8d0093604
AliiMuhammed <mohamedtaha8000@gmail.com>
2025-01-27 15:49:46 +0200

    add Tailwind CSS and implement Navbar and Home components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router";
 import MoveToTop from "./Shared/MoveToTop";
+import Navbar from "./Shared/Navbar";
 
 function App() {
   return (
-    
+
     <>
+      <Navbar />
       <Outlet />
       <MoveToTop />
     </>
