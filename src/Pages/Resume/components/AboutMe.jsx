5ec103acb6d1ed55d0289666ec941a0f0de19551
AliiMuhammed <mohamedtaha8000@gmail.com>
2025-03-03 17:40:40 +0200

    feat: enhance AboutMe component with new layout and styling; add personal information and links

--- src/Pages/Resume/components/AboutMe.jsx
+++ src/Pages/Resume/components/AboutMe.jsx
@@ -1,13 +1,51 @@
 import React from "react";
-
+import "../style/about.css";
+import { Link } from "react-router-dom";
 const AboutMe = () => {
   return (
     <div className="about-container">
       <div className="header">
-        <h1>My courses & certifications</h1>
+        <h1>ِAbout me</h1>
         <p>
-          My coursework has provided me with a diverse skill set and in-depth
-          knowledge, equipping me to excel in my field.
+          Driven by curiosity and a passion for growth, I strive to bring
+          dedication and creativity to everything I do
+        </p>
+      </div>
+
+      <div className="info-container">
+        <p className="info">
+          <span>name</span>Ali Muhammed Ahmed
+        </p>
+        <p className="info">
+          <span>phone</span>(+20) 01066567630
+        </p>
+        <p className="info">
+          <span>experience</span>+1 years
+        </p>
+        <p className="info">
+          <span>freelance</span>Available
+        </p>
+        <p className="info">
+          <span>linkedIn</span>
+          <Link
+            to={"https://www.linkedin.com/in/ali-muhammed-dev"}
+            target="_blank"
+          >
+            Ali Muhammed
+          </Link>
+        </p>
+        <p className="info">
+          <span>nationality</span>Egyptian
+        </p>
+        <p className="info">
+          <span>email</span>
+          <Link to={"mailto:ali.muhammed.dev@gmail.com"}>
+            ali.muhammed.dev@gmail.com
+          </Link>
+        </p>
+        <p className="info">
+          <span>language</span>
+          Arabic, English
         </p>
       </div>
     </div>
@@ -15,4 +53,3 @@ const AboutMe = () => {
 };
 
 export default AboutMe;
-ٍٍ~~~
