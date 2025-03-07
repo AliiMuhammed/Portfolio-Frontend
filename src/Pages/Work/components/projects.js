38fe4fb5731cd55fa2eee785f92a55ce7ffeec3f
AliiMuhammed <mohamedtaha8000@gmail.com>
2025-03-07 07:05:26 +0200

    feat: update link for Bistro Bliss project in Work component; direct to new template URL

--- src/Pages/Work/components/projects.js
+++ src/Pages/Work/components/projects.js
@@ -10,7 +10,7 @@ const projects = [
         }
         ,
         description: "Bistro Bliss is a handy web app for food lovers. Explore menus, book tables effortlessly, and save your reservations with an account. Dining out made easy!",
-        link: "https://aliimuhammed.netlify.app/",
+        link: "https://bistro-bliss-template.netlify.app/",
         github: "https://github.com/AliiMuhammed/Final-Project-Amit",
         techStack: ["React.js", "Node.js", "Express", "MongoDB", "HTML5", "CSS3", "JavaScript", "Redux"],
         video: "",
