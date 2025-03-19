e01616782818ac19304efbf711adef1e5c8e5b14
AliiMuhammed <mohamedtaha8000@gmail.com>
2025-03-19 23:13:45 +0200

    feat: implement dynamic experience loading in Experience component; add loading state and error handling

--- src/Pages/Resume/components/Experience.jsx
+++ src/Pages/Resume/components/Experience.jsx
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../style/experience-education.css";
+import Spinner from "../../../Shared/Spinner";
+import { client } from "../../../Client";
 const Experience = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [ex, setEx] = useState([]);
+  useEffect(() => {
+    const query = '*[_type=="experience"]';
+    client
+      .fetch(query)
+      .then((response) => {
+        const sortedExperiences = response.sort(
+          (a, b) => new Date(b.startDate) - new Date(a.startDate)
+        );
+        setEx(sortedExperiences);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching projects:", error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  const formatDate = (dateString) => {
+    if (!dateString) return "Present";
+    const date = new Date(dateString);
+    return date.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "short",
+    });
+  };
   return (
     <div className="experience-container">
       <div className="header">
@@ -10,31 +39,23 @@ const Experience = () => {
           integrating AI models into real-time applications.
         </p>
       </div>
-      <div className="cards">
-        {[
-          {
-            title: "Coding Instructor",
-            company: "iSchool",
-            date: "Jun 2024 - Oct 2024",
-          },
-          {
-            title: "Online Coordinator",
-            company: "Almentor",
-            date: "Dec 2023 - Jul 2024",
-          },
-          {
-            title: "Front-end Developer",
-            company: "freelancer",
-            date: "Aug 2023 - Oct 2023 ",
-          },
-        ].map((content, index) => (
-          <div key={index} className="card">
-            <p className="date">{content.date}</p>
-            <h2 className="title">{content.title}</h2>
-            <h3 className="company">{content.company}</h3>
-          </div>
-        ))}
-      </div>
+      {isLoading && ex.length === 0 && <Spinner size={"3"} color={"light"} />}
+      {!isLoading && ex.length > 0 && (
+        <div className="cards">
+          {!isLoading && ex.length === 0 && (
+            <p>No experience found. Please check back later.</p>
+          )}
+          {ex.map((content, index) => (
+            <div key={index} className="card">
+              <p className="date">{`${formatDate(
+                content.startDate
+              )} - ${formatDate(content.endDate)}`}</p>
+              <h2 className="title">{content.title}</h2>
+              <h3 className="company">{content.company}</h3>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
